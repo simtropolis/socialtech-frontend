a1593155008a6d1f30994b3ffbe31e01bd0c4f17
Ahmad Vegah <ahmadvegah@gmail.com>
2021-11-30 18:16:09 +0000

    simplified layout and added relevant text.

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -10,7 +10,7 @@ const Banner = () => (
       title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
       subtitle="Start your Free Trial."
       button={
-        <Link href="https://creativedesignsguru.com/category/nextjs/">
+        <Link href="#">
           <a>
             <Button>Get Started</Button>
           </a>
