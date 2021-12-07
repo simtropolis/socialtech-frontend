4b6a41dcd17eb0dc7204fd51cd1e43ed47e4edce
Ahmad Vegah <ahmadvegah@gmail.com>
2021-12-07 14:40:39 +0000

    made changes to tsconfig to fix pre commit errors

---
+++ urls.js
@@ -0,0 +1,19 @@
+module.exports = {
+  Apply: 'https://revolt.chat/beta',
+  PublicBeta: 'https://revolt.chat/publicbeta',
+  GenerateCode: 'https://campaign.revolt.chat/api/revolt.chat',
+  ApplicationForm: 'https://revolt.chat/publicbeta',
+  App: 'https://app.revolt.chat',
+  Developers: 'https://developers.revolt.chat',
+  ProjectTracker: 'https://github.com/orgs/revoltchat/projects/3/views/1',
+
+  Feedback: 'https://github.com/revoltchat/revolt/discussions',
+  SourceCode: 'https://gitlab.insrt.uk/revolt',
+  Reddit: 'https://reddit.com/r/revoltchat',
+  Twitter: 'https://twitter.com/revoltchat',
+  Mastodon: 'https://mastodon.social/@revoltapp',
+  GitHub: 'https://github.com/revoltchat',
+  GitLab: 'https://gitlab.insrt.uk/revolt',
+
+  Translate: 'https://weblate.insrt.uk/engage/revolt/?utm_source=widget',
+};
